62e7a945774a5a86537d9b97ac9111e9b257ca77
gzalinger <gzalinger@gmail.com>
2014-03-10 14:10:08 -0400

    final conf change;

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 
 function love.conf(t)
 	 t.title = "Kastel"
-	 t.screen.fullscreen = false
-	 t.screen.width = 1024
-	 t.screen.height = 800
+	 --t.screen.fullscreen = false
+	 --t.screen.width = 1024
+	 --t.screen.height = 800
          t.window.fullscreen = false
          t.window.width = 1024
          t.window.height = 768
