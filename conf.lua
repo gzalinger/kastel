754e3e56460b84480db45ffbf5047fd58de4c494
gzalinger <gzalinger@gmail.com>
2014-03-10 14:07:34 -0400

    changed conf to work on windows

--- conf.lua
+++ conf.lua
@@ -1,7 +1,10 @@
 
 function love.conf(t)
 	 t.title = "Kastel"
-	 t.screen.fullscreen = false
-	 t.screen.width = 1024
-	 t.screen.height = 800
+	-- t.screen.fullscreen = false
+	-- t.screen.width = 1024
+	-- t.screen.height = 800
+        t.window.fullscreen = false
+        t.window.width = 1024
+        t.window.height = 768
 end
